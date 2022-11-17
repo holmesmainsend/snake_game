ec75b63162ca660c4d2f8e153abf4e8df48bbd65
Mona Lisa <holmesmainsend@gmail.com>
2022-11-17 18:16:00 -0500

    began adding initscr code

--- gamecode.c
+++ gamecode.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <curses.h>
 
 int main() {
+    initscr();
+    
+    clear();
+    move(10,20);
+    addstr("Hello, world");
+    move(LINES-1,0);
 
+    refresh();
+    getch();
+
+    endwin();
 }
