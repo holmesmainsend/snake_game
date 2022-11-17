025040e80b54317bc8833eafed99298be299e009
Mona Lisa <holmesmainsend@gmail.com>
2022-11-17 18:29:40 -0500

    added arrow key recognition

--- gamecode.c
+++ gamecode.c
@@ -5,14 +5,32 @@
 
 int main() {
     initscr();
-    
     clear();
-    move(10,20);
-    addstr("Hello, world");
-    move(LINES-1,0);
-
-    refresh();
-    getch();
-
-    endwin();
+    curs_set(0);
+    noecho();
+    keypad(stdscr,TRUE);
+    move(0,0);
+    addstr("Press any key...");
+    while(1) {
+        int direction = getch();
+        move(0,0);
+        switch(direction) {
+            case KEY_UP:
+                addstr("You pressed the UP arrow");
+                break;
+            case KEY_DOWN:
+                addstr("You pressed the DOWN arrow");
+                break;
+            case KEY_LEFT:
+                addstr("You pressed the LEFT arrow");
+                break;
+            case KEY_RIGHT:
+                addstr("You pressed the RIGHT arrow");
+                break;
+            case 'q':
+                endwin();
+                return 0;
+        }
+        refresh();
+    }
 }
