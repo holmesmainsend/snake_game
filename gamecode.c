f4558de44eb79a0af44aba75768a824656ac8b21
Mona Lisa <holmesmainsend@gmail.com>
2022-11-17 18:06:31 -0500

    removed test code

--- gamecode.c
+++ gamecode.c
@@ -3,20 +3,5 @@
 #include <curses.h>
 
 int main() {
-    int x;
-    printf("Please enter a numeric grade: ");
-    scanf("%d", &x);
-    if (x < 0 || x > 100)
-        printf("Your input was invalid.");
-    else if (x < 101 && x >= 90)
-        printf("Your grade is an A.");
-    else if (x < 90 && x >= 80)
-        printf("Your grade is a B.");
-    else if (x < 80 && x >= 70)
-        printf("Your grade is a C.");
-    else if (x < 70 && x >= 60)
-        printf("Your grade is a D.");
-    else
-        printf("Your grade is an F.");
-    return 0;
+
 }
