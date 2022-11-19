f9e914a3b0c7a8d5d7186cf025d2592b034ea198
Mona Lisa <holmesmainsend@gmail.com>
2022-11-19 13:20:55 -0500

    updated comments

--- gamecode.c
+++ gamecode.c
@@ -8,16 +8,18 @@
 #include <unistd.h>
 
 int main() {
-    // Explanation of this code added below
+    // Explanation of these variables added below
     char snake[999];
     char eraser[999];
     char temporary[999];
+
     strcpy(snake, "----->");
     strcpy(eraser, "      ");
     
     int y = 0;
     int x = 0;
 
+    // Initializing ncurses screen
     initscr();
     clear();
     curs_set(0);
@@ -42,9 +44,12 @@ int main() {
         move(y,x);
         addstr(eraser);
         
-        //I believe that we can use if(user input changes) then (run the switch code) 
-        //That way we can continually move the snake even when there is no input
-        //Also we can store the direction instead of updating x and y and increment the head of the snake by the direction
+        /*
+            I believe that we can use if(user input changes) then (run the switch code) 
+            That way we can continually move the snake even when there is no input
+            Also we can store the direction instead of updating x and y and increment 
+            the head of the snake by the direction
+        */
         int key = getch();
         switch(key) {
             case KEY_UP:
@@ -64,10 +69,11 @@ int main() {
                 return 0;
         }
         /*
+            Snake growth code:
             As far as I could determine, C does not contain a simple way to add characters to the beginning
             of an array (like the unshift() method in JavaScript). Therefore, the code below transfers the
             current snake to a temporary placeholder, sets a "-" segment as the new snake array, and then
-            appends the rest of the former snake to this by copying it over from the snakeTemp. The same
+            appends the rest of the former snake to this by copying it over from temporary. The same
             process is repeated for the eraser of the old snake, except with a " " instead of a "-" char.
             Currently, the rate of growth is one snake segment per key press.
         */
