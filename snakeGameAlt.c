007e1e0c17431b03631c023acc8b455a4e204dcc
Mona Lisa <holmesmainsend@gmail.com>
2022-11-29 18:09:12 -0500

    updated TODOs

--- snakeGameAlt.c
+++ snakeGameAlt.c
@@ -1,9 +1,10 @@
-// TODO: record screencast of final gameplay
+// TODO: record screencast demo of final gameplay
 // TODO: add comments to finished program code
-// OPTIONAL: add colors and style features
+
+// OPTIONAL: add colors and style features, win screen, etc.
+// OPTIONAL: implement scoring and score display
 // TODO: add win condition (snake's length reaches half the perimeter of the border)
-// TODO: implement scoring and score display
-// TODO: add trophy expiration time (1 to 9 seconds randomly)
+// TODO: add trophy expiration time (1 to 9 seconds randomly) and account for trophy out of range of snake
 // TODO: start snake with random direction
 // TODO: implement increased speed when snake gets longer
 // TODO: fix growth after trophy eating
